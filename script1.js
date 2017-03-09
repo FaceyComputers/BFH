7044c2055ad759ca4a4486c1842cbf0f43a3890f
Mickey Killoran <mickeykilloran@gmail.com>
2017-03-09 15:33:36 -0700

    Switched back to old js for testing
    This version works locally

--- script1.js
+++ script1.js
@@ -14,44 +14,44 @@ var ifopen_off = false;
 var ifopen_second = false;
 
 function onClick() {
-    if(boolean == false)
-    {
-    document.getElementById("fdsa").style.visibility = "visible";
-    document.getElementById("fdsa").style.display = "block";
-    document.body.style.overflow = "hidden";
-    document.body.style.overflowY = "hidden";
-    document.body.style.position = "fixed";
-    document.body.height = "100%";
-    boolean = true;
-    }else{
-    document.getElementById("fdsa").style.visibility = "hidden"; 
-    document.getElementById("fdsa").style.display = "none";
-    document.body.style.position = "relative";
-    document.body.style.overflow = "visible";
-    boolean_Academics = false;
-    boolean_Options = false;
-    boolean_Students = false;
-    boolean_Athletics = false;
-    boolean_Supports = false;    
-    boolean = false;
-    document.getElementById("scrolldownplease").style.height = "auto";   
-    boolean_Academics = false;
+    "use strict";
+    if (boolean === false) {
+        document.getElementById("fdsa").style.visibility = "visible";
+        document.getElementById("fdsa").style.display = "block";
+        document.body.style.overflow = "hidden";
+        document.body.style.overflowY = "hidden";
+        document.body.style.position = "fixed";
+        document.body.height = "100%";
+        boolean = true;
+    } else {
+        document.getElementById("fdsa").style.visibility = "hidden";
+        document.getElementById("fdsa").style.display = "none";
+        document.body.style.position = "relative";
+        document.body.style.overflow = "visible";
+        boolean_Academics = false;
+        boolean_Options = false;
+        boolean_Students = false;
+        boolean_Athletics = false;
+        boolean_Supports = false;
+        boolean = false;
+        document.getElementById("scrolldownplease").style.height = "auto";
+        boolean_Academics = false;
     }
 }
 
-function Setup()
-{
+function Setup() {
+    "use strict";
     var string = document.getElementsByClassName('active')[0].innerHTML;
     var int = string.search(">");
     string = string.substr(int + 1);
-    var int = string.search("<");
+    int = string.search("<");
     string = string.substring(int, -1);
     document.title = string + " | Bev Facey Open House";
     
 }
 
-function setHeight(text)
-{
+function setHeight(text) {
+    "use strict";
     var element = document.getElementById('scrolldownplease');
     var style = window.getComputedStyle(element);
     var top = style.getPropertyValue('height');
@@ -63,26 +63,23 @@ function setHeight(text)
     boolean_Community = false;
     
     
-    if(boolean_Academics == true || boolean_Options == true || boolean_Students == true || boolean_Athletics == true || boolean_Supports == true)
-    {
+    if (boolean_Academics === true || boolean_Options === true || boolean_Students === true || boolean_Athletics === true || boolean_Supports === true) {
         boolean_Academics = true;
     }
     
-    if(boolean_Academics == false)
-    {
-    var height = window.innerHeight;
-    height = height - 100;
-    var string = height + "px";
-    document.getElementById("scrolldownplease").style.height = string; 
-    boolean_Academics = true;
-    }else{
-    document.getElementById("scrolldownplease").style.height = "auto";   
-    boolean_Academics = false;
+    if (boolean_Academics === false) {
+        var height = window.innerHeight - 100;
+        var string = height + "px";
+        document.getElementById("scrolldownplease").style.height = string;
+        boolean_Academics = true;
+    } else {
+        document.getElementById("scrolldownplease").style.height = "auto";
+        boolean_Academics = false;
     }
 }
 
-function setHeightOptions()
-{
+function setHeightOptions() {
+    "use strict";
     var element = document.getElementById('scrolldownplease');
     var style = window.getComputedStyle(element);
     var top = style.getPropertyValue('height');
@@ -93,21 +90,20 @@ function setHeightOptions()
     boolean_Supports = false;
     boolean_Community = false;
     
-    if(boolean_Options == false)
-    {
-    var height = window.innerHeight;
-    height = height - 100;
-    var string = height + "px";
-    document.getElementById("scrolldownplease").style.height = string; 
-    boolean_Options = true;
-    }else{
-    document.getElementById("scrolldownplease").style.height = "auto";   
-    boolean_Options = false;
+    if (boolean_Options === false) {
+        var height = window.innerHeight;
+        height = height - 100;
+        var string = height + "px";
+        document.getElementById("scrolldownplease").style.height = string;
+        boolean_Options = true;
+    } else {
+        document.getElementById("scrolldownplease").style.height = "auto";
+        boolean_Options = false;
     }
 }
 
-function setHeightStudent()
-{
+function setHeightStudent() {
+    "use strict";
     var element = document.getElementById('scrolldownplease');
     var style = window.getComputedStyle(element);
     var top = style.getPropertyValue('height');
@@ -118,18 +114,25 @@ function setHeightStudent()
     boolean_Supports = false;
     boolean_Community = false;
     
-    if(boolean_Students == false)
-    {
-    var height = window.innerHeight;
-    height = height - 100;
-    var string = height + "px";
-    document.getElementById("scrolldownplease").style.height = string; 
-    boolean_Students = true;
-    }else{
-    document.getElementById("scrolldownplease").style.height = "auto";   
-    boolean_Students = false;
+    if (boolean_Students === false) {
+        var height = window.innerHeight;
+        height = height - 100;
+        var string = height + "px";
+        document.getElementById("scrolldownplease").style.height = string;
+        boolean_Students = true;
+    } else {
+        document.getElementById("scrolldownplease").style.height = "auto";
+        boolean_Students = false;
     }
 }
+
+/*$(document).bind('mobileinit', function(){ 
+    $.mobile.metaViewportContent = 'width=device-width'; 
+});*/
+
+/*$(document).bind('mobileinit', function(){ 
+    $.mobile.metaViewportContent = 'width=device-width, minimum-scale=1, maximum-scale=2'; 
+});
 
 function setHeightAthletics()
 {
@@ -341,31 +344,33 @@ $(function () {
 $(function () {
 
     $('.notClose-second').on('click', 'a', function (e) {
-            event.stopPropagation();
-
-            $(".thirds-cts").hide();
-            $(".thirds-other").hide();
-            $(".thirds-fa").hide();
-            $(".thirds-off").hide();
-            $(".thirds-second").hide();
-            if(ifopen_cts == true || ifopen_other == true || ifopen_fa == true || ifopen_off == true || ifopen_second == true)
-            {
-            ifopen_cts = false;
-            ifopen_other = false;
-            ifopen_fa = false;
-            ifopen_off = false;
-            }
-
-        if(ifopen_second == false)
+    event.stopPropagation();
+        
+        $(".thirds-cts").hide();
+        $(".thirds-other").hide();
+        $(".thirds-fa").hide();
+        $(".thirds-off").hide();
+        $(".thirds-second").hide();
+        if(ifopen_cts == true || ifopen_other == true || ifopen_fa == true || ifopen_off == true || ifopen_second == true)
         {
-            $( this ).parents(".dropdown-submenus").addClass("open"); 
-            $(".thirds-second").show();
-            ifopen_second = true;
-        }else{
-            $( this ).find(".dropdown-submenus").removeClass("open");  
-            $(".thirds-second").hide();
-            ifopen_second = false;
+        ifopen_cts = false;
+        ifopen_other = false;
+        ifopen_fa = false;
+        ifopen_off = false;
         }
+        
+    if(ifopen_second == false)
+    {
+        $( this ).parents(".dropdown-submenus").addClass("open"); 
+        $(".thirds-second").show();
+        ifopen_second = true;
+    }else{
+        $( this ).find(".dropdown-submenus").removeClass("open");  
+        $(".thirds-second").hide();
+        ifopen_second = false;
+    }
     });
 
 });
+
+*/
